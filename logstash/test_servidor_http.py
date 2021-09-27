41530612b18364a2c8201d867f77bda3d4452afd
LINUX <jmoc25@gmail.com>
2021-09-27 14:16:02 +0200

    curso_elk

--- logstash/test_servidor_http.py
+++ logstash/test_servidor_http.py
@@ -9,3 +9,20 @@ webclient.send(bytes("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n".encode('utf-8')
 reply = webclient.recv(4096)
 print("Response from %s:" % webhost)
 print(reply.decode())
+
+import requests
+
+url = 'http://localhost:8081'
+
+my_data = {
+    'message': 'this is the message'
+}
+
+headers = requests.utils.default_headers()
+headers.update(
+    {
+        'User-Agent': 'My User Agent 1.0',
+    }
+)
+
+response = requests.post(url, headers=headers,data=my_data['message'])
